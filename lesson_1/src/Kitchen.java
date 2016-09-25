3fb531fc80fdb1fe2ec515421373b8c35a2a167a
luminiks <luminiksik@yandex.ru>
2016-09-25 20:28:17 +0300

    rename + add new class "cookSoup "

---
+++ lesson_1/src/Kitchen.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Liminiksik on 25.09.2016.
+ */
+
+public class Kitchen {
+    void cookSoup(){
+        // Ctrl -D  - дублирование строки
+        System.out.println("Boil water");
+        System.out.println("-------");
+        System.out.println("done");
+    }
+
+}
--- lesson_1/src/Main.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Liminiksik on 25.09.2016.
- */
-public class Main {
-}
