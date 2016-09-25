17b26ae70b162c0128e39feb118e6e8c795801fc
luminiks <luminiksik@yandex.ru>
2016-09-25 21:56:18 +0300

    change

--- lesson_1/src/Kitchen.java
+++ lesson_1/src/Kitchen.java
@@ -6,6 +6,7 @@ public class Kitchen {
     public static void main(String[] args){
         System.out.println("main");
         cookBreakfast();
+        cookSoup();
     }
 
     static void cookSoup(){
