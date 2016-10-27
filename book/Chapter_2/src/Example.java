dc941c6395562fa85e6afd5d0bd0fcd0a1a32ac3
luminiks <luminiksik@yandex.ru>
2016-10-27 14:23:44 +0300

    book of Java
    add Example2

--- book/Chapter_2/src/Example.java
+++ book/Chapter_2/src/Example.java
@@ -1,9 +1,8 @@
-    /*
-        Это простая прогромма на Java.
-        Присвоить исходному файлу имя "Example.java"
-     */
+/*
+    Это простая прогромма на Java.
+    Присвоить исходному файлу имя "Example.java"
+ */
 public class Example {
-
     // Это программа начинается с вызоова метода main()
     public static void main(String args[]){
         System.out.println("Простая программа на java.");
